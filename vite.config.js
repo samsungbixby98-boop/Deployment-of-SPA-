64d36c1d5eed367522fb8477cb3b1628b5344a9b
samsungbixby98-boop <samsungbixby98@gmail.com>
2026-03-05 18:15:52 +0530

    hopee

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Deployment-of-SPA-/",
   build: {
     target: 'es2018',
     minify: 'esbuild',
